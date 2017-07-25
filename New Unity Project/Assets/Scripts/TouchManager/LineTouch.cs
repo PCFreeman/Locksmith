f1a4d570316be3e98c50a44de113ef127a134feb
Rafael Salles <rafaelcsalles@hotmail.com>
2017-07-24 21:46:32 -0300

    Collision
    Collision is working now.

---
+++ New Unity Project/Assets/Scripts/TouchManager/LineTouch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineTouch : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<SpriteRenderer>().color = new Color(0.1f, 0.0f, 0.0f, 1.0f);
+        Debug.Log("Collides!!");
+    }
+}
