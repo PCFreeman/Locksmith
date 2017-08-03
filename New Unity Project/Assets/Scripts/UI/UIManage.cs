89c3653bcf8027ab155add6492c4cc71b5b92f25
PCFreeman <freemanfrank96@gmail.com>
2017-08-03 13:27:49 -0700

    2017-08-03 Fuck my life
    addScore and addTime funciton, i leave the rest to saturday

---
+++ New Unity Project/Assets/Scripts/UI/UIManage.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManage : MonoBehaviour {
+
+    private static UIManage instance;
+    public float timeLeft;
+    public Text Timer;
+    public Text Point;
+    public GameObject Set;
+
+    float Mins;
+    float Secs;
+
+   public int Score=0;
+ 
+
+    public void settingMenu()
+    {
+        Set.SetActive(true);
+        Time.timeScale = 0f;
+    }
+   public void SettingMenuBack()
+    {
+        Set.SetActive(false);
+        Time.timeScale = 1f;
+    }
+ 
+    public void AddScore(int pluse)
+    {
+       Score=Score + pluse;
+    }
+
+    public void AddTime(int T)
+    {
+        timeLeft = timeLeft + T;
+    }
+
+    void Update()
+    {
+        Mins = Mathf.FloorToInt(timeLeft / 60f);
+        Secs = Mathf.FloorToInt(timeLeft % 60f);
+  
+
+        if (timeLeft>0)
+        {
+          timeLeft -= Time.deltaTime;
+    
+                Timer.text = "" + Mins + ":" + Secs;
+        }
+
+        if(timeLeft==0)
+        {
+            timeLeft = 0;
+        }
+    }
+}
